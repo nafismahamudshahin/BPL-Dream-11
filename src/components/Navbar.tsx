430d7cb6fb1513b7c2e6f6aaf84acf27ca2dbb99
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-08 00:16:39 +0600

    create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import { BsCoin } from "react-icons/bs";
+import Logo from "../assets/logo.png";
+const Navbar = () => {
+    return (
+        <nav className="my-6">
+            <div className="container mx-auto flex justify-between items-center">
+                <div>
+                    {/* logo */}
+                    <img src={Logo} alt="site logo" />
+                </div>
+                <div className="">
+                    <ul className="flex gap-10 items-center">
+                        <li className="text-[#131313] cursor-pointer">Home</li>
+                        <li className="text-[#131313] cursor-pointer">Fixture</li>
+                        <li className="text-[#131313] cursor-pointer">Teams</li>
+                        <li className="text-[#131313] cursor-pointer">Schedules</li>
+                        <li className="btn">0 Coin<BsCoin className="text-yellow-500" /></li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
