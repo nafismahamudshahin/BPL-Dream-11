089b8b9b4f7c48cbe34af1bf52caba874f345a16
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-10 04:45:18 +0600

    change newsletter background

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,8 +1,8 @@
-
+import BgShadow from "../assets/bg-shadow.png";
 const Newsletter = () => {
     return (
         <section className="mt-20 -mb-40 relative z-10">
-            <div className="container mx-auto bg-[rgba(188,195,198,0.35)] rounded-2xl border-3 border-[rgba(56,189,248,0.35)]">
+            <div style={{ backgroundImage: `${BgShadow}`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }} className="container mx-auto">
                 <div className="flex flex-col justify-center py-20 m-3 rounded-2xl bg-white bg-[radial-gradient(circle_at_0%_100%,rgba(56,189,248,0.35),transparent_35%),radial-gradient(circle_at_100%_0%,rgba(251,146,60,0.35),transparent_35%)]">
                     <div className="text-center space-y-3">
                         <h3 className="text-3xl text-center font-semibold">Subscribe to our Newsletter</h3>
